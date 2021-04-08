4c43d9ccbac637c61c6819fe1abff71e757c2ca6
Jane Wilson <janewils@email.unc.edu>
2021-04-08 02:46:42 -0700

    Wind update

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,11 +1,15 @@
 /* reference what you need */
+var doc = document;
+console.log(doc);
 var toggleButton = document.querySelector(".toggle");
 var links = document.querySelector(".links");
 
 /* desired behavior */
 function toggle() {
   links.classList.toggle("active");
+  alert("Hi!");
 }
 
 /* wire it up */
 toggleButton.addEventListener("click", toggle);
+
